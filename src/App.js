c15348240a2c1bd136fc0fe656bf9db7a5d9ac45
Kaustubh Manglurkar <46368989+KaustubhM1997@users.noreply.github.com>
2022-04-05 02:22:41 +0530

    moved to components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
+import { Nav } from "./components/Nav/Nav";
 import { ParentRouter } from "./Routes/routes";
 
 function App() {
   return (
     <div className="App">
+      <Nav/>
       <ParentRouter />
     </div>
   );
--- src/pages/Home/home.jsx
+++ src/pages/Home/home.jsx
@@ -1,65 +1,10 @@
 import "./home.css";
-import { Link } from "react-router-dom";
+import { Aside } from "../../components/Aside/Aside";
 
 const Home = () => {
   return (
     <>
-      <nav className="nav-container">
-        <Link className="link-style" to= "/"> 
-        <div className="brand-name">
-          Pro<span style={{ color: "black" }}>NOTES</span>{" "}
-        </div>
-        </Link>
-        
-        <div className="nav-input">
-          <input placeholder="Search items" type="search" />
-        </div>
-        <div className="nav-cta">
-          <a className="primary-btn" href="">
-            Login
-          </a>
-        </div>
-      </nav>
-
-      <div className="child-wrapper">
-        <aside className="aside-wrapper">
-          <ul className="aside-list">
-            <li className="aside-item aside-item-active">
-              {" "}
-              <i class="fa-solid fa-house aside-icons"></i>{" "}
-              <a className="aside-link active" href="/">
-                Home
-              </a>
-            </li>
-            <li className="aside-item">
-              {" "}
-              <i class="fa-solid fa-tags aside-icons"></i>
-              <a className="aside-link" href="/labels">
-                Label
-              </a>
-            </li>
-            <li className="aside-item">
-              {" "}
-              <i class="fa-solid fa-box-archive aside-icons"></i>
-              <a className="aside-link" href="/archive">
-                Archives
-              </a>
-            </li>
-            <li className="aside-item active">
-              {" "}
-              <i class="fa-solid fa-trash aside-icons"></i>
-              <a className="aside-link" href="/trash">
-                Trash
-              </a>
-            </li>
-          </ul>
-
-          {/* <Link>Profile</Link> */}
-
-          <button className="create-note-btn">Create Note</button>
-        </aside>
-        <div className="content-wrapper"></div>
-      </div>
+      <Aside />
     </>
   );
 };
