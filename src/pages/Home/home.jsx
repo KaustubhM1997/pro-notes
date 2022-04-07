d9b84da5d198f9aa97ed8f193778a52b0dc6e784
Kaustubh Manglurkar <46368989+KaustubhM1997@users.noreply.github.com>
2022-04-07 14:32:44 +0530

    added: note-card

--- src/pages/Home/home.jsx
+++ src/pages/Home/home.jsx
@@ -1,7 +1,18 @@
 import "./home.css";
 
 const Home = () => {
-  return <div>Home again!</div>;
+  return (
+    <div className="main-wrapper">
+      <div className="note-wrapper">
+        <form className="notes-input-form" action="">
+          <input type="text" placeholder="Title" />
+          <textarea
+            placeholder="Take a note"
+          ></textarea>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export { Home };
