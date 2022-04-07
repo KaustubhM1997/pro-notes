65a429f5fe45b6067faf26c3edad99eb04af8fed
Kaustubh Manglurkar <46368989+KaustubhM1997@users.noreply.github.com>
2022-04-08 02:44:17 +0530

    added: card-css

--- src/pages/Home/home.jsx
+++ src/pages/Home/home.jsx
@@ -6,9 +6,16 @@ const Home = () => {
       <div className="note-wrapper">
         <form className="notes-input-form" action="">
           <input type="text" placeholder="Title" />
-          <textarea
-            placeholder="Take a note"
-          ></textarea>
+          <textarea placeholder="Take a note"></textarea>
+          <div className="cta-buttons">
+            <div className="operations">
+              <i class="fa-solid fa-palette operation-btn"></i>
+              <i class="fa-solid fa-tags operation-btn-2"></i>
+            </div>
+
+              <button type="submits" className="notes-btn">Add Note</button>
+            
+          </div>
         </form>
       </div>
     </div>
